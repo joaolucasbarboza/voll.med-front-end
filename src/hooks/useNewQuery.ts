e5742d890ae51caed6aed7a4466e54293a542bae
João L. Barboza <joaolucasbarbozaa@gmail.com>
2024-05-16 21:48:56 -0300

    feat: Hook para marcar uma nova consulta.

---
+++ src/hooks/useNewQuery.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+
+export function useNewQuery() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return {
+    form,
+    onSubmit,
+  }
+}
