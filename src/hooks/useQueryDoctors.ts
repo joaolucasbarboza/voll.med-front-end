6710bcfcc09b3e71b287b021d4d6ed2bac768cc0
João L. Barboza <joaolucasbarbozaa@gmail.com>
2024-05-16 21:49:24 -0300

    feat: Hook para buscar o Médico.

---
+++ src/hooks/useQueryDoctors.ts
@@ -0,0 +1,41 @@
+import { api } from "@/services/api";
+import { useEffect, useState } from "react";
+
+export type DoctorType = {
+  foto: object;
+  id: string;
+  nome: string;
+  email: string;
+  crm: string;
+  especialidade: string;
+}
+
+interface DoctorsResponse {
+  content: DoctorType[];
+}
+
+export function useQueryDoctors() {
+  const [dataDoctors, setDataDoctors] = useState<DoctorsResponse | null>(null);
+  const [isLoadingDoctors, setIsLoading] = useState(true);
+
+  function fetchData() {
+    api
+      .get("/medicos")
+      .then((response) => {
+
+        setDataDoctors(response.data);
+
+        setIsLoading(false);
+      })
+      .catch((error) => console.error(error));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return {
+    dataDoctors,
+    isLoadingDoctors,
+  };
+}
